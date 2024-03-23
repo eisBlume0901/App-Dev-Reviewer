61c769db11a8d152f0f850633991097e1a68435c
Mary Kate Anecito <marykate09@protonmail.com>
2024-03-23 21:52:08 +0800

    Add more string methods to practice all of it

--- 4_Playing_With_Strings/index.js
+++ 4_Playing_With_Strings/index.js
@@ -1,19 +1,37 @@
 const userString = document.querySelector('#user-string');
 const stringWithIndex = document.querySelector('#string-with-index');
-const checkButton = document.querySelector('#check-button');
-const showStringResults = () => {
+const checkButton = document.querySelector('button[type=submit]');
+const stringInfo = document.querySelector('#string-info');
+const userName = document.querySelector('#user-name');
+const greetUserLabel = document.querySelector('#greet-user');
+const showStringWithIndex = () => {
     const stringVal = userString.value;
     let stringValWithSpaces = "";
     let stringWithIndexVal = "";
     for (let i = 0; i < stringVal.length; i++) {
-        stringValWithSpaces += stringVal[i] + " ";
+        stringValWithSpaces += stringVal.charAt(i) + " "
     }
     for (let i = 0; i < stringVal.length; i++) {
         stringWithIndexVal += i + " ";
     }
 
-
     stringWithIndex.innerHTML = `${stringValWithSpaces}<br>${stringWithIndexVal}`;
 };
 
-checkButton.addEventListener('click', showStringResults);
+const showStringInfo = () => {
+    const stringVal = userString.value;
+    stringInfo.innerHTML = `String: ${stringVal}<br>Length: ${stringVal.length}`;
+}
+
+const greetUser = () => {
+    const userNameVal = userName.value;
+    greetUserLabel.innerHTML = `Hello, ${userNameVal}`;
+}
+checkButton.addEventListener('click', () => {
+    greetUser();
+    showStringInfo();
+    showStringWithIndex();
+
+    greetUserLabel.value = "";
+    userString.value = "";
+});
